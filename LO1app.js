f7f4b0a3a5b6e92b004a594b44a80b6dd1e793b7
Riconeeeee <riconeometer18@gmail.com>
2024-10-03 13:46:27 +0800

    initial commit

---
+++ LO1app.js
@@ -0,0 +1,11 @@
+//Input
+const person1 ={
+    name:"Ricone Ometer",
+    age:15,
+    occupation:"Wala"
+}
+
+//Process and Output
+console.log("Name: "+person1.name);
+console.log("Age: "+person1.age);
+console.log("Occupation: "+person1.occupation);
